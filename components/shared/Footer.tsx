4a3f31b246848f6bbf7486b6e0c5899585e8d0d8
Denitsa Slavkova <deni9309@gmail.com>
2024-03-26 19:37:35 +0200

    Integrate the Footer in the app layout

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="wrapper flex flex-col flex-center flex-between gap-4 p-5 text-center text-sm sm:flex-row">
+        <Link href="/">
+          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={38} />
+        </Link>
+
+        <p>2024 Evently - All Rights Reserved</p>
+      </div>
+    </footer>
   );
 };
 
