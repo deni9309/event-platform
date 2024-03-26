1ac74f19dd706c940ed78ba31c7dfe15305e3b9f
Denitsa Slavkova <deni9309@gmail.com>
2024-03-27 00:15:28 +0200

    Add hero on home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from 'react';
 
-const Home = () => {
+export default function Home() {
   return (
-    <div>
-      
-      <h1 className="text-4xl">Home</h1></div>
-  )
-}
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-conatin py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host, Connect, Celebrate Your Events With Evently!</h1>
 
-export default Home
+            <p className="p-regular-20 md:p-regular-24">
+              A leading event platform, easy-to-use and with the lowest booking fees for event creation and fundraising pages.
+            </p>
+            <Button asChild size="lg" className="button w-full sm:w-fit " >
+              <Link href="#events">Explore Now</Link>
+            </Button>
+          </div>
+
+          <Image src="/assets/images/hero-3.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70dvh] object-contain object-center 2xl:max-h-[70dvh]"
+          />
+        </div>
+      </section>
+
+      <section id="events"
+        className="wrapper flex flex-col gap-8 md:gap-12 my-8">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex flex-col md:flex-row gap-5 w-full">
+          Search
+          Category Filter
+        </div>
+      </section>
+    </>
+  );
+};
+//className="m-auto w-auto"
