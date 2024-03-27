0428dcf51750603d9a14fba0e27e4dc645501168
Denitsa Slavkova <deni9309@gmail.com>
2024-03-27 20:00:08 +0200

    Add 'User', 'Event' and 'Category' models.

---
+++ lib/database/models/event.model.ts
@@ -0,0 +1,39 @@
+import { Document, model, models, Schema } from "mongoose";
+
+import {ICategory } from './category.model';
+import { IUser } from "./user.model";
+
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: string;
+  isFree: Boolean;
+  url?: string;
+  category: string | ICategory;
+  organizer: string | IUser;
+}
+
+const EventSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  location: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  imageUrl: { type: String, required: true },
+  startDateTime: { type: Date, default: Date.now },
+  endDateTime: { type: Date, default: Date.now },
+  price: { type: String },
+  isFree: { type: Boolean, default: false },
+  url: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  organizer: { type: Schema.Types.ObjectId, ref: 'User' },
+});
+
+const Event = models.Event || model('Event', EventSchema);
+
+export default Event;
