253298cf626a05901e7d407d9bb1575a8f48dabf
Denitsa Slavkova <deni9309@gmail.com>
2024-03-28 17:19:14 +0200

    Format code

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,9 +4,9 @@ import { revalidatePath } from "next/cache";
 import { CreateUserParams, UpdateUserParams } from "@/types";
 import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
-import User from "../database/models/user.model";
-import Event from "../database/models/event.model";
-import Order from "../database/models/order.model";
+import User from "@/lib/database/models/user.model";
+import Event from "@/lib/database/models/event.model";
+import Order from "@/lib/database/models/order.model";
 
 export async function createUser(user: CreateUserParams) {
   try {
--- lib/utils.ts
+++ lib/utils.ts
@@ -11,18 +11,12 @@ export function cn(...inputs: ClassValue[]) {
 export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryParams) {
   const currentUrl = qs.parse(params);
 
-  keysToRemove.forEach(key => {
-    delete currentUrl[key];
+  keysToRemove.forEach(key => { delete currentUrl[key]; });
+
+  return qs.stringifyUrl({ url: window.location.pathname, query: currentUrl }, {
+    skipNull: true
   });
-
-  return qs.stringifyUrl({
-    url: window.location.pathname,
-    query: currentUrl
-  },
-    { skipNull: true }
-  );
 }
-
 
 export const handleError = (error: unknown) => {
   console.error(error);
