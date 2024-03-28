3bd290251f65ee6a981a73882bdeac8a010a7ca6
Denitsa Slavkova <deni9309@gmail.com>
2024-03-28 19:11:03 +0200

    create 'events' folder in (root) - add create and update pages in it; create EventForm reusable component;

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
 
-export default function RootLayout({children}: {children: React.ReactNode;}) {
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <div className="flex h-screen flex-col">
       <Header />
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
 import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -40,4 +41,3 @@ export default function Home() {
     </>
   );
 };
-//className="m-auto w-auto"
