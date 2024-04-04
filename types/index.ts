5dc6ef218ff9be226c60d4990f8e930c3d1af390
Denitsa Slavkova <deni9309@gmail.com>
2024-04-04 19:16:13 +0300

    Fix 'CreateOrderParams' type bug

--- types/index.ts
+++ types/index.ts
@@ -118,7 +118,7 @@ export type CreateOrderParams = {
   eventId: string;
   buyerId: string;
   totalAmount: string;
-  createdAt: string;
+  createdAt: Date;
 };
 
 export type GetOrdersByEventParams = {
